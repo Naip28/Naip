c86677f659462d2732f1fae270b35f12c3cade8f
5 <miroslavoikulyk@gmail.com>
2024-05-05 18:29:06 +0300

    45

--- NIGA/Scrap_Mechanic/urls.py
+++ NIGA/Scrap_Mechanic/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.helloworld, name='helloworld'),
-    path('gg/', views.rock, name='rock')
+    path('gg/', views.rock, name='rock'),
+    path('gorin/', views.gorin, name='gorin'),
+    path('reg/', views.reg, name='reg'),
+    path('vxod/', views.vxod, name='vxod'),
 ]
--- NIGA/Scrap_Mechanic/views.py
+++ NIGA/Scrap_Mechanic/views.py
@@ -4,4 +4,14 @@ def helloworld(request):
     return render(request, 'helloworld.html')
 
 def rock(request):
-    return render(request, 'rock.html')
+    return render(request, 'rock.html')\
+
+def gorin(request):
+    return render(request, 'gorin.html')
+
+def reg(request):
+    return render(request, 'reg.html')
+
+def vxod(request):
+    return render(request, 'vxod.html')
+
